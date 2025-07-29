e3638ac781f1e26922d4042057d5a754ca181c5e
Soumyadeep <173267965+SimpleMan05@users.noreply.github.com>
2025-07-30 00:53:30 +0530

    favicon change

--- src/components/AppComponent/header.tsx
+++ src/components/AppComponent/header.tsx
@@ -12,7 +12,7 @@ function Header()
         <header className = "sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur py-2 supports-[backdrop-filter]:bg-background/60">
             <div className = "container mx-auto flex h-16 items-center justify-between px-4">
                 <Link to  = {"/"}>
-                <img src={theme == "dark" ? "../../public/logo_dark.png" : "../../public/logo_light.png"} 
+                <img src={theme == "dark" ? "../../public/logo_dark2.png" : "../../public/logo_light.png"} 
                 alt="Klimate Logo" className="h-14" />
                 </Link>
 
