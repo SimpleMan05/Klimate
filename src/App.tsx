8232c496f8241dd9439547a9f83e874edc1eefe6
Soumyadeep <173267965+SimpleMan05@users.noreply.github.com>
2025-07-30 00:37:12 +0530

    App structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,25 @@
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './index.css';
-
+import Layout from './components/AppComponent/layout.tsx'
+import { ThemeProvider } from './components/theme-provider.tsx';
+import Dashboard from './pages/dashboard.tsx';
+import City from './pages/city.tsx'
 function App() {
 
   return (
-    <>
     <BrowserRouter>
-    
+      <ThemeProvider>
+        <Layout>
+          <Routes>
+            <Route path = '/' element = {<Dashboard />} ></Route>
+            <Route path = '/city/:cityName' element = {<City />} ></Route>
+            <Route path = '/' element = {<Dashboard />} ></Route>
+
+          </Routes>
+        </Layout>
+      </ThemeProvider>
     </BrowserRouter>
-    </>
-  )
+  );
 }
 
 export default App
