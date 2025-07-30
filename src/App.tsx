7a6e09b8a1ee58e21120de93cf4b8859828bc95b
Soumyadeep <173267965+SimpleMan05@users.noreply.github.com>
2025-07-30 17:27:30 +0530

    tanstack query setup

--- src/App.tsx
+++ src/App.tsx
@@ -4,21 +4,39 @@ import Layout from './components/AppComponent/layout.tsx'
 import { ThemeProvider } from './components/theme-provider.tsx';
 import Dashboard from './pages/dashboard.tsx';
 import City from './pages/city.tsx'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'  //devtools
+
+import {
+  useQuery,
+  useMutation,
+  useQueryClient,
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+
+
+
 function App() {
 
-  return (
-    <BrowserRouter>
-      <ThemeProvider>
-        <Layout>
-          <Routes>
-            <Route path = '/' element = {<Dashboard />} ></Route>
-            <Route path = '/city/:cityName' element = {<City />} ></Route>
-            <Route path = '/' element = {<Dashboard />} ></Route>
+  const queryClient = new QueryClient();
 
-          </Routes>
-        </Layout>
-      </ThemeProvider>
-    </BrowserRouter>
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <ThemeProvider>
+          <Layout>
+            <Routes>
+              <Route path = '/' element = {<Dashboard />} ></Route>
+              <Route path = '/city/:cityName' element = {<City />} ></Route>
+              <Route path = '/' element = {<Dashboard />} ></Route>
+
+            </Routes>
+          </Layout>
+        </ThemeProvider>
+      </BrowserRouter>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 }
 
